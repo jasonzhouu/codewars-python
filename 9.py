03c7fdd1717c9610be7d8b8c7e3304e539f9ff3c
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-03-12 23:46:47 +0800

    use lambda function, and set()

--- 9.py
+++ 9.py
@@ -1,9 +1,4 @@
-def is_isogram(string):
-    string = string.lower()
-    for i in string:
-        if string.count(i) > 1:
-            return False
-    return True
+is_isogram = lambda string: len(string) == len(set(string.lower()))
 
 print(is_isogram("mqizrzax"))
 print(is_isogram("helo"))
