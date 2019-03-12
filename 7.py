d16a59d358401d4e42a51e45fbf5ede59a266708
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-03-12 21:44:26 +0800

    use "".join() function

--- 7.py
+++ 7.py
@@ -1,3 +1,4 @@
-repeat_str = lambda string, repeat: string * repeat
+def repeat_str(string, repeat):
+    return "".join([string]*repeat)
 
-print(repeat_str("hello ", 5))
+print(repeat_str("hello", 5))
