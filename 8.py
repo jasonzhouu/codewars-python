b8f26df6c3f245deb9192592f2408150cc37f0ad
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-03-12 22:04:32 +0800

    reversed(string)
    reversed() 函数不仅可以用在数组上，还可以用在字符串上，得到一个反转后
    的数组

--- 8.py
+++ 8.py
@@ -1,7 +1,7 @@
 def solution(string):
     result = ""
-    for i in range(len(string)-1, -1, -1):
-        result += string[i]
+    for i in reversed(string):
+        result += i
     return result
 
 print(solution("hello"))
