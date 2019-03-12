9e79541cf4cf0439f9e0c5bd34d4f35d543decc0
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-03-12 21:30:13 +0800

    sum odd with sum() and range()

--- 6/6.py
+++ 6/6.py
@@ -1,11 +1,4 @@
 def row_sum_odd_numbers(n):
-    start = int(n * (n-1) / 2)
-    sum = 0
-    i = 1
-    while( i <= n ):
-        sum += (start * 2 + 1)
-        i += 1
-        start += 1
-    return sum
+    return sum(range(n*(n-1)+1, n*(n+1), 2))
 
 print(row_sum_odd_numbers(41))
