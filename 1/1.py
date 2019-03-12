5d229daa24ee4af3675cd8125023e3cf3a4b9e49
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-03-12 12:59:18 +0800

    count sheep

---
+++ 1/1.py
@@ -0,0 +1,16 @@
+def count_sheeps(arrayOfSheeps):
+    sum = 0
+    for sheep in arrayOfSheeps:
+        if sheep == True:
+            sum += 1
+    return sum
+
+array1 = [True,  True,  True,  False,
+          True,  True,  True,  True ,
+          True,  False, True,  False,
+          True,  False, False, True ,
+          True,  True,  True,  True ,
+          False, False, True,  True ];
+
+print(count_sheeps(array1))
+
