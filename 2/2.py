7d737504b9eb3ffc49e31e0fc785bee2c77c0ee8
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-03-12 13:35:45 +0800

    use builtin min/max

--- 2/2.py
+++ 2/2.py
@@ -1,10 +1,11 @@
-import builtins
+omin = min
+omax = max
 
 def max(input_array):
-    return builtins.max(input_array)
+    return omax(input_array)
 
 def min(input_array):
-    return builtins.min(input_array)
+    return omin(input_array)
 
 x = [1, 2, -1, 10]
 print(max(x))
