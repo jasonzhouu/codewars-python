2318025d1fce83d0ac54aca5dc48aa14483b0778
Yusheng Zhou <jasonzhouu@163.com>
2019-03-13 17:08:54 +0800

    success, except when there is other symbols

--- 13.py
+++ 13.py
@@ -1,11 +1,12 @@
 def find_secret_message(paragraph):
-    li1 = paragraph.lower().replace(".", "").split(" ")
-    li2 = list(set(li1))
-    print(li1)
-    print(li2)
-    for i in li2:
-        li1.remove(i)
-    return " ".join(li1)
+    li = paragraph.lower().replace(".", "").split(" ")
+    result = []
+    for index, value in enumerate(li):
+        li_before = li[:index]
+        if value in li_before:
+            if value not in result:
+                result.append(value)
+    return " ".join(result)
 
 
 paragraph = 'This is a test. this test is fun.'
