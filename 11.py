b6353edf2abfa2c9f9c0e846f1d7d6506959a715
Yusheng Zhou <jasonzhouu@163.com>
2019-03-13 12:33:07 +0800

    arithmetic

---
+++ 11.py
@@ -0,0 +1,11 @@
+def arithmetic(a, b, operator):
+    if operator ==  "add":
+        return a + b
+    if operator == "subtract":
+        return a - b
+    if operator == "multiply":
+        return a * b
+    if operator == "divide":
+        return a / b
+    else:
+        "invalid input"
