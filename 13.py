31ff9155d0c26ec8c5678441263c6d6a3f0d5386
Yusheng Zhou <jasonzhouu@gmail.com>
2019-03-13 17:49:33 +0800

    more compact

--- 13.py
+++ 13.py
@@ -1,7 +1,6 @@
 def find_secret_message(paragraph):
-    li = [i.strip('.,:!?') for i in paragraph.lower().split(" ")]
     result = []
-    for index, value in enumerate(li):
+    for index, value in enumerate([i.strip('.,:!?') for i in paragraph.lower().split(" ")]):
         li_before = li[:index]
         if value in li_before and value not in result:
             result.append(value)
