ddd6944a0fb479906a89ea42d1b233b91de3ecff
Yusheng Zhou <jasonzhouu@163.com>
2019-03-13 12:58:12 +0800

    use package operator

--- 11.py
+++ 11.py
@@ -1,11 +1,13 @@
-def arithmetic(a, b, operator):
-    if operator ==  "add":
-        return a + b
-    if operator == "subtract":
-        return a - b
-    if operator == "multiply":
-        return a * b
-    if operator == "divide":
-        return a / b
-    else:
-        "invalid input"
+import operator
+
+def arithmetic(a, b,operator_name):
+    operators = {
+        "add": operator.add,
+        "subtract": operator.sub,
+        "multiply": operator.mul,
+        "divide": operator.truediv
+    }
+    return operators[operator_name](a,b)
+
+print(arithmetic(1, 2, 'add'))
+print(arithmetic(1, 2, 'divide'))
