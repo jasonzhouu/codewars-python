63e0524e616c3c6afbfbb5168b68983be1ffde4c
Yusheng Zhou <jasonzhouu@163.com>
2019-03-13 17:39:51 +0800

    使用strip()函数去除单词后的标点符号

--- 13.py
+++ 13.py
@@ -1,13 +1,13 @@
 def find_secret_message(paragraph):
-    li = paragraph.lower().replace(".", "").split(" ")
+    li = paragraph.lower().split(" ")
+    li = [i.strip('.,:!?') for i in li]
     result = []
     for index, value in enumerate(li):
         li_before = li[:index]
-        if value in li_before:
-            if value not in result:
-                result.append(value)
+        if value in li_before and value not in result:
+            result.append(value)
     return " ".join(result)
 
 
-paragraph = 'This is a test. this test is fun.'
+paragraph = 'This is a test. this test is fun.,:!?.'
 print(find_secret_message(paragraph))
